e3d7a1e13752b224bbb84dcb4d9e5aa237ec857a
audxkawjdzhsks <qmrfjr@gmail.com>
2020-11-12 18:50:09 +0900

    3test
    3test

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.2/17253
 // Filename: silhouette_v6.ggsk
-// Generated 월 11 9 13:38:09 2020
+// Generated 목 11 12 18:47:54 2020
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
